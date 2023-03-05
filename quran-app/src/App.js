ace8551d4c3a96f5436e6f30ff2dabb5d84cf673
Abdiwali-Eid <cabdiwalibashir232@gmail.com>
2023-03-05 21:02:49 +0300

    first commit

---
+++ quran-app/src/App.js
@@ -0,0 +1,14 @@
+import Home from './components/Home'
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="text-center">
+      <Home/>
+       
+     </h1>
+    </div>
+  );
+}
+
+export default App;
